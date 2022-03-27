d24eed04086449f35bbf72609621d6783e8ad9e7
GustavoCarv <gustavo.carvalho@econverse.com.br>
2022-03-27 20:21:13 -0300

    style: header estilizado para todos os dispositivos

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,3 +1,55 @@
-import Header from "./index";
+import Image from "next/image";
+import logo from "../../../public/assets/shared/logo.svg";
+import { useState } from "react";
+
+const Header = () => {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleMobileMenu = (): void => {
+    console.log(isActive);
+    setIsActive(!isActive);
+  };
+  return (
+    <header className=" primary-header flex ">
+      <div className="logo">
+        <Image src={logo} alt="logo"></Image>
+      </div>
+      <button
+        className={"mobile-nav-toggle" + (isActive ? " is--active" : "")}
+        onClick={handleMobileMenu}
+        aria-controls="primary-navigation"
+      >
+        <span
+          className="sr-only menu-mobile"
+          aria-expanded={isActive ? "true" : "false"}
+        >
+          Menu
+        </span>
+      </button>
+      <nav>
+        <ul
+          id="primary-navigation"
+          className={
+            "flex primary-navigation underline-indicators" +
+            (isActive ? " is--active" : "")
+          }
+        >
+          <li className="active ff-sans-cond uppercase text-white letter-spacing-2">
+            <span>00 </span> Home
+          </li>
+          <li className="ff-sans-cond uppercase text-white letter-spacing-2">
+            <span>01 </span> Destination
+          </li>
+          <li className="ff-sans-cond uppercase text-white letter-spacing-2">
+            <span>02 </span> Crew
+          </li>
+          <li className="ff-sans-cond uppercase text-white letter-spacing-2">
+            <span>03 </span> Technology
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
